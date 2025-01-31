4f57e84833b507c6a8c1688d1ca1b934e56e7643
Anonymous2024-spec <shifahnana2@gmail.com>
2025-01-31 10:28:34 +0300

    render updates

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -14,6 +15,6 @@ def get_info():
         "current_datetime": datetime.utcnow().isoformat() + "Z",
         "github_url": "https://github.com/Anonymous2024-spec/hng12-public-api.git"
     })
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Get the port from Render, default to 5000
+    app.run(host='0.0.0.0', port=port)
